2b68e75194b648888825c0c1ba9298ad3d06e9d7
Igor Lima <igorlimaisl7@gmail.com>
2025-03-06 16:16:59 -0300

    projeto.js

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,15 @@
-//tipos de variaveis:
-let idade = 90 // tipo number
-let nome = 'Igor Lima' //tipo string
-let legado = false //tipo boolean
 
-/// array 
-/*let ingredientes = ["farinha","agua" ,"corante","sal"]
+/*
+tipos de variaveis:
+let idade = 90  tipo number
+let nome = "Igor" tipo string
+let legado = false tipo boolean
+
+ array 
+let ingredientes = ["farinha","agua" ,"corante","sal"]
 
 console.log(ingredientes[9])
-//objetos
+objetos
 let personagem = {
     nome: "Igor Lima",
     nivel : 10,
@@ -19,19 +21,19 @@ let personagem = {
 
 console.log(personagem.magia)
 
-//funçoes 
+funçoes 
 function somar( a, b ){
     let resultado = a + b 
     return resultado
 }
 
-let x1 = somar( 15 , 5 ) //15 é A e 5 é B RESULTADO DESSA FUNÇÃO VAI SER 20 SE FOR SOMAR
+let x1 = somar( 15 , 5 ) 15 é A e 5 é B RESULTADO DESSA FUNÇÃO VAI SER 20 SE FOR SOMAR
 let y2 = somar(2,8)
 console.log(x1)
 console.log(y2)
 
 
-/// Arronw Fuctions
+ Arronw Fuctions
 let somar2 = (a,b) => {
     let resultado = a + b 
     return resultado
@@ -40,10 +42,40 @@ let somar2 = (a,b) => {
 let x = somar(15,5)
 let y = somar2(2,8)
 console.log(x)
-console.log(y)*/
+console.log(y)
 
-//// Condicionais de Execução
+ Condicionais de Execução
 
-if (idade >= 18){
-    console.log("Você é maior de idade")
+if (idade == 18){
+    console.log("Parabens agora você pode ser Preso")
+
 }
+simbolos de comparação
+ == 
+ !=
+> 
+<=
+>= maior ou igual
+if (nome == "Igor"){
+console.log("mensagem especial")
+} else {
+    console.log("Mensagem Normal")
+}
+
+lista = ["massa", "farinha","massa"]
+
+for(let item of lista){
+    console.log("-  ingredientes " + item)
+}
+*/
+
+//DOM
+//alert("opa aquii está um aviso")
+
+//Gerenciar Eventos 
+//Evento de click é quando vai acontecer
+function avisar(){
+    alert("Opa, Disparei")
+}
+
+////cONTINUAR EM 1 :15 ,56 ESTAVA EXPLICANDO SOBRE ONCLICKS 
