5cdb1fc88645daa5ddc94c15c2d1cd6bfe69ff70
Igor Lima <igorlimaisl7@gmail.com>
2025-03-06 12:34:00 -0300

    aula.js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,26 @@
-let idade = 90
-let nome = 'Igor Lima'
-let legado = false
+//tipos de variaveis:
+let idade = 90 // tipo number
+let nome = 'Igor Lima' //tipo string
+let legado = false //tipo boolean
 
-///
+/// array 
+let ingredientes = ["farinha","agua" ,"corante","sal"]
+
+console.log(ingredientes[9])
+//objetos
+let personagem = {
+    nome: "Igor Lima",
+    nivel : 10,
+    forca: 800,
+    magia : 200,
+    vida : 2000,
+    mana : 200
+}
+
+console.log(personagem.magia)
+
+//funçoes 
+function somar( a, b ){
+    let resultado = a + b 
+    return resultado
+}
