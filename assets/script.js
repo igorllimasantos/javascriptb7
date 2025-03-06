29b799db7035a7c630ee8fd6b73366bbd585b521
Igor Lima <igorlimaisl7@gmail.com>
2025-03-06 14:28:15 -0300

    projeto.js

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ let nome = 'Igor Lima' //tipo string
 let legado = false //tipo boolean
 
 /// array 
-let ingredientes = ["farinha","agua" ,"corante","sal"]
+/*let ingredientes = ["farinha","agua" ,"corante","sal"]
 
 console.log(ingredientes[9])
 //objetos
@@ -23,4 +23,27 @@ console.log(personagem.magia)
 function somar( a, b ){
     let resultado = a + b 
     return resultado
+}
+
+let x1 = somar( 15 , 5 ) //15 é A e 5 é B RESULTADO DESSA FUNÇÃO VAI SER 20 SE FOR SOMAR
+let y2 = somar(2,8)
+console.log(x1)
+console.log(y2)
+
+
+/// Arronw Fuctions
+let somar2 = (a,b) => {
+    let resultado = a + b 
+    return resultado
+}
+
+let x = somar(15,5)
+let y = somar2(2,8)
+console.log(x)
+console.log(y)*/
+
+//// Condicionais de Execução
+
+if (idade >= 18){
+    console.log("Você é maior de idade")
 }
